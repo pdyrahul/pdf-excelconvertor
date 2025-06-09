2858c5416df1fe1608f7b87eff3cf6491b132266
rahul <rahulpdy.pandey@gmail.com>
2025-06-10 01:49:45 +0530

    portadded

--- app.py
+++ app.py
@@ -92,4 +92,6 @@ def index():
     return render_template("index.html", data=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
